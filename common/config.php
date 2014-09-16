51e3b67cf1d77e2a94a9067a5dd3508b2a20cc32
loneblue <loneblue@naver.com>
2014-09-16 14:03:32 +0900

    기존 소스 이동동

---
+++ common/config.php
@@ -0,0 +1,7 @@
+<?
+#### DB 관련 변수 정의 ####
+$db_server="localhost"; 
+$db_user="geonkorea";           // Database login ID
+$db_passwd="wjdtlsckflwk";       // Database password
+$db_name="geonkorea";           // Database name
+?>
